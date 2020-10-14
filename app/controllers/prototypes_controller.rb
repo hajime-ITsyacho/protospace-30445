e90024ccbdf1faadae2dc2329a022402d6a01bea
hajime-ITsyacho <hyokei614@gmail.com>
2020-10-14 19:37:18 +0900

    編集機能の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @prototypes = Prototype.all 
   end
@@ -49,8 +50,9 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    if user_signed_in?
-      redirect_to action: :index
+    unless user_signed_in?
+      redirect_to action: :index      
     end
   end
+
 end
